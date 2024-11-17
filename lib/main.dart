22e30c15133f7b8770804e5c338938d258c6d762
Philopater_Nabil <fylwbatyrnbylnasr@gmail.com>
2024-11-17 18:14:40 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:clean_arch_bookly_app/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      title: 'Clean Arch Bookly App',
+      home: SplashView(),
+    );
+  }
+}
