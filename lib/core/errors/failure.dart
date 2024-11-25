1d77e5faf70ddc53d064c69034590cc21eab7e7d
Philopater_Nabil <fylwbatyrnbylnasr@gmail.com>
2024-11-26 01:40:09 +0200

    Refactor error handling and integrate Dio exceptions in HomeRepoImpl
    - Introduced an abstract `Failure` class to serve as a base for different types of failures.
    - Created specific failure classes: `ServerFailure`, `CacheFailure`, and `NetworkFailure`.
    - Implemented `ServerFailure.fromDioError` to handle various Dio exceptions and map them to appropriate failure messages.
    - Updated `HomeRepoImpl` to use the new failure classes, specifically handling `DioException` errors and converting them into `ServerFailure` instances.
    - Enhanced error messages for better clarity and user feedback.

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,5 +1,61 @@
-class Failure {
+import 'package:dio/dio.dart';
+
+abstract class Failure {
   final String message;
 
   Failure(this.message);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.message);
+
+  factory ServerFailure.fromDioError(DioException error) {
+    switch (error.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection timeout with ApiServer');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send timeout with ApiServer');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive timeout with ApiServer');
+      case DioExceptionType.badCertificate:
+        return ServerFailure('BadCertificate with ApiServer');
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+          error.response!.statusCode,
+          error.response!.data,
+        );
+      case DioExceptionType.cancel:
+        return ServerFailure('Request to ApiServer is canceled');
+      case DioExceptionType.connectionError:
+        return ServerFailure('Connection Error with ApiServer');
+      case DioExceptionType.unknown:
+        return ServerFailure('Unexpected Error, Please try again!');
+    }
+  }
+
+  factory ServerFailure.fromResponse(int? statusCode, dynamic response) {
+    if (statusCode != null) {
+      switch (statusCode) {
+        case 400:
+          return ServerFailure(response['error']['message']);
+        case 401:
+          return ServerFailure(response['error']['message']);
+        case 403:
+          return ServerFailure(response['error']['message']);
+        case 404:
+          return ServerFailure('Your request not found, Please try later!');
+        case 500:
+          return ServerFailure('There is a problem with the server, Please try later!');
+      }
+    }
+    return ServerFailure('There is a problem with the server, Please try later!');
+  }
+}
+
+class CacheFailure extends Failure {
+  CacheFailure(super.message);
+}
+
+class NetworkFailure extends Failure {
+  NetworkFailure(super.message);
 }
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -4,6 +4,7 @@ import 'package:clean_arch_bookly_app/features/home/data/data_sources/home_remot
 import 'package:clean_arch_bookly_app/features/home/domain/entities/book_entity.dart';
 import 'package:clean_arch_bookly_app/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl extends HomeRepo {
   final HomeRemoteDataSource homeRemoteDataSource;
@@ -24,7 +25,10 @@ class HomeRepoImpl extends HomeRepo {
         return Right(books);
       }
     } catch (e) {
-      return Left(Failure(e.toString()));
+      if (e is DioException) {
+        return Left(ServerFailure.fromDioError(e));
+      }
+      return Left(ServerFailure(e.toString()));
     }
   }
 
@@ -40,7 +44,10 @@ class HomeRepoImpl extends HomeRepo {
         return Right(books);
       }
     } catch (e) {
-      return Left(Failure(e.toString()));
+      if (e is DioException) {
+        return Left(ServerFailure.fromDioError(e));
+      }
+      return Left(ServerFailure(e.toString()));
     }
   }
 }
