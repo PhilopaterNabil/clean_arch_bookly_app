912fdf43f6cdf2634a51a99bcfc2c5b240af5cdd
Philopater_Nabil <fylwbatyrnbylnasr@gmail.com>
2024-11-25 23:58:14 +0200

    Add HomeRemoteDataSource implementation for fetching books
    Implement the `HomeRemoteDataSource` abstract class with concrete methods to fetch featured and newest books from the API. The `HomeRemoteDataSourceImpl` class uses the `ApiService` to make GET requests to the appropriate endpoints and maps the JSON response to a list of `BookEntity` objects. The `getBooksList` method is a helper function to convert the API response into a list of `BookModel` instances, which are then returned as `BookEntity` objects.

---
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -0,0 +1,38 @@
+import 'package:clean_arch_bookly_app/core/utils/api_service.dart';
+import 'package:clean_arch_bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:clean_arch_bookly_app/features/home/domain/entities/book_entity.dart';
+
+abstract class HomeRemoteDataSource {
+  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
+}
+
+class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
+  final ApiService apiService;
+
+  HomeRemoteDataSourceImpl(this.apiService);
+  @override
+  Future<List<BookEntity>> fetchFeaturedBooks() async {
+    final data = await apiService.get(
+        endPoint: 'volumes?Filtering=free-ebooks&q=subject:Programming');
+    List<BookEntity> books = getBooksList(data);
+    return books;
+  }
+
+  @override
+  Future<List<BookEntity>> fetchNewestBooks() async {
+    final data = await apiService.get(
+        endPoint:
+            'volumes?Filtering=free-ebooks&Sorting=newest&q=subject:Programming');
+    List<BookEntity> books = getBooksList(data);
+    return books;
+  }
+
+  List<BookEntity> getBooksList(Map<String, dynamic> data) {
+    List<BookEntity> books = [];
+    for (var item in data['items']) {
+      books.add(BookModel.fromJson(item));
+    }
+    return books;
+  }
+}
