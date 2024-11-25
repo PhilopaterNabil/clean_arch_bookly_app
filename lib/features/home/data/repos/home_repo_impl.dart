ed851eb00e23bc0abb9ff9e7d6cb73a9c3ddc99f
Philopater_Nabil <fylwbatyrnbylnasr@gmail.com>
2024-11-26 01:08:54 +0200

    Add HomeRepoImpl class for fetching featured and newest books
    This commit introduces the `HomeRepoImpl` class, which implements the `HomeRepo` interface. The class is responsible for fetching featured and newest books from both local and remote data sources. If the local data source contains the requested books, they are returned immediately; otherwise, the remote data source is queried. The implementation uses the `dartz` package to handle errors and return `Either` types, ensuring that failures are properly propagated.

---
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,46 @@
+import 'package:clean_arch_bookly_app/core/errors/failure.dart';
+import 'package:clean_arch_bookly_app/features/home/data/data_sources/home_local_data_source.dart';
+import 'package:clean_arch_bookly_app/features/home/data/data_sources/home_remote_data_source.dart';
+import 'package:clean_arch_bookly_app/features/home/domain/entities/book_entity.dart';
+import 'package:clean_arch_bookly_app/features/home/domain/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl extends HomeRepo {
+  final HomeRemoteDataSource homeRemoteDataSource;
+  final HomeLocalDataSource homeLocalDataSource;
+
+  HomeRepoImpl(
+      {required this.homeRemoteDataSource, required this.homeLocalDataSource});
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() async {
+    try {
+      List<BookEntity> books;
+      books = homeLocalDataSource.fetchFeaturedBooks();
+      if (books.isNotEmpty) {
+        return Right(books);
+      } else {
+        books = await homeRemoteDataSource.fetchFeaturedBooks();
+        return Right(books);
+      }
+    } catch (e) {
+      return Left(Failure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() async {
+    try {
+      List<BookEntity> books;
+      books = homeLocalDataSource.fetchNewestBooks();
+      if (books.isNotEmpty) {
+        return Right(books);
+      } else {
+        books = await homeRemoteDataSource.fetchNewestBooks();
+        return Right(books);
+      }
+    } catch (e) {
+      return Left(Failure(e.toString()));
+    }
+  }
+}
