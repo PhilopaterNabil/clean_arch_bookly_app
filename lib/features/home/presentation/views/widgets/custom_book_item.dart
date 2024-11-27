7df5964e282079178ef418f75d9bb2004869cc26
Philopater_Nabil <fylwbatyrnbylnasr@gmail.com>
2024-11-27 21:28:08 +0200

    Update CustomBookItem widget to use CachedNetworkImage and add sqflite plugin
    - Modified `CustomBookItem` widget to use `CachedNetworkImage` for loading book images from a network URL instead of a local asset. This change improves performance and reduces the need for local image assets.
    - Added `sqflite` plugin to the macOS project, enabling SQLite database support for the application.
    - Updated `pubspec.yaml` to include the `cached_network_image` package and its dependencies, ensuring compatibility and functionality with the new image loading approach.
    - Updated `pubspec.lock` to reflect the new dependencies and their versions.

--- lib/features/home/presentation/views/widgets/custom_book_item.dart
+++ lib/features/home/presentation/views/widgets/custom_book_item.dart
@@ -1,23 +1,21 @@
-import 'package:clean_arch_bookly_app/core/utils/asssets.dart';
-import 'package:clean_arch_bookly_app/core/utils/color_manager.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CustomBookItem extends StatelessWidget {
-  const CustomBookItem({super.key});
+  const CustomBookItem({super.key, required this.image});
+
+  final String image;
 
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 2.7 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-          image: const DecorationImage(
-            fit: BoxFit.cover,
-            image: AssetImage(Asssets.kBookImage),
-          ),
-          borderRadius: BorderRadius.circular(16.r),
-          color: ColorManager.whiteColor,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16.r),
+        child: CachedNetworkImage(
+          imageUrl: image,
+          fit: BoxFit.cover,
         ),
       ),
     );
